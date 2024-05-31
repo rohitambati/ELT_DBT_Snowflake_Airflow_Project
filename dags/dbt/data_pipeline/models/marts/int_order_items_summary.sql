18d2971cacf5472dd586b6e33adcfe9d69e80de9
rohitambati <rohitambati2@gmail.com>
2024-05-30 22:50:35 -0500

    Initial commit

---
+++ dags/dbt/data_pipeline/models/marts/int_order_items_summary.sql
@@ -0,0 +1,10 @@
+{{ config(materialized='table', sort='timestamp', dist='user_id') }}
+
+select
+    order_key,
+    sum(extended_price) as gross_item_sales_amount,
+    sum(item_discount_amount) as item_discount_amount
+from
+    {{ ref('int_order_items') }}
+group by
+    order_key
